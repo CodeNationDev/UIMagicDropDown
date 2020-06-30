2e6c2bb581fc674c79fc9b66af4c631ee8cf30de
David Martin Saiz <davms81@gmail.com>
2020-06-30 12:41:15 +0200

    Test SPM path

--- Package.swift
+++ Package.swift
@@ -24,6 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UIMagicDropDown",
-            dependencies: [])
+            dependencies: [],
+            path: "Sources")
     ]
 )
